84cd4ae7eb487c91f041e837c3a4974e15d3f6c2
Jasmine McManus <jasmine.mcmanus@outlook.com>
2023-06-08 15:15:54 +1000

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+
+const cats = [ 'Milo', 'Otis', 'Garfield'];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+
+function appendCat(name) {
+    const cats2 = [...cats, name];
+    return cats2
+}
+
+function prependCat(name) {
+    const cats3 = [name, ...cats];
+    return cats3
+}
+
+function removeLastCat() {
+    const cats4 = cats.slice(0, 2);
+    return cats4;
+}
+
+function removeFirstCat() {
+    const cats5 = cats.slice(1);
+    return cats5;
+}
